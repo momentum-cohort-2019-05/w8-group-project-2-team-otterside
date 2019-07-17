84afb33db16290e3e3346ab1feb7d2ebdb3156dd
Michael <phamm89@gmail.com>
2019-07-17 09:24:44 -0400

    Added User to Admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from core.models import Snippet
+from core.models import Snippet, AbstractUser
 
 # Models registered for Code Snippet 
 
  # Register the Admin classes for BookAuthor using the decorator
 @admin.register(Snippet)
 class SnippetAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AbstractUser)
+class User(admin.ModelAdmin):
     pass
