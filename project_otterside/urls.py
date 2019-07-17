f8be081a38d51dec7e072be5fe7ed2ba80f3cbcb
Michael <phamm89@gmail.com>
2019-07-17 07:29:10 -0400

    Added to Models

--- project_otterside/urls.py
+++ project_otterside/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from core import views as core_views
 
 urlpatterns = [
+    path('', core_views.index, name='index'), 
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
