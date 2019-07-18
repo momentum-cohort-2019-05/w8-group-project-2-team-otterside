a2460df51200abe7c3ab58f5fb0321895592f4cf
christopherwburke <christopher.w.burke@gmail.com>
2019-07-18 15:26:43 -0400

    Added snippet form

---
+++ core/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.db.models import TextField
+from .models import Snippet, CustomUser
+
+class SnippetForm(forms.ModelForm):
+
+    class Meta:
+        model = Snippet
+        fields = ('snippet_title', 'snippet_lang', 'snippet_code', 'snippet_description', 'snippet_creator', 'date_added',)
