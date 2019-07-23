07c774428359f6d795edd4341632e996a20cec09
Michael <phamm89@gmail.com>
2019-07-23 12:25:23 -0400

    Django Heroku

--- project_otterside/settings.py
+++ project_otterside/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 # Configure Django App for Heroku.
 import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -181,3 +181,5 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
+# Configure Django for Heroku
+django_heroku.settings(locals())
