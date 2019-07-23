c70c279d1823b216810c28ba9945f2d866105017
Michael <phamm89@gmail.com>
2019-07-23 09:11:54 -0400

    Installing Heroku

--- project_otterside/settings.py
+++ project_otterside/settings.py
@@ -177,3 +177,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
